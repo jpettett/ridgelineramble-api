e00a12768afbfe7b296c9db0f767907d776f1acd
jpettett <joseph.pettett@gmail.com>
2020-03-11 14:34:18 -0700

    added comma

--- ridge_ramble_django/settings.py
+++ ridge_ramble_django/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
