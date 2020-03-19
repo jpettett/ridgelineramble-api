110ee0db66011f65bf8fdc511d8e57ad1adde754
jpettett <joseph.pettett@gmail.com>
2020-03-19 14:11:48 -0700

    updated JWT

--- ridge_ramble/permissions.py
+++ ridge_ramble/permissions.py
@@ -8,5 +8,4 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        # Write permissions are only allowed to the owner of the snippet.
-        return obj.owner == request.user
+        
